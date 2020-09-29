648295c74c3c80b12c290dacb94a349d32b670c2
Aram Mikayelyan <aram_mikayelyan@tutanota.com>
2020-09-29 22:11:26 +0400

    create database connection pool

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 // Define an application struct to hold the application-wide
@@ -17,12 +20,22 @@ type application struct {
 func main() {
 	// Parsing the runtime configs for the app.
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:pass123!@/snippetbox?parseTime=true", "MySQL dsn")
 	flag.Parse()
 
 	// Creating a logger for writing information and error
 	// messages to stdout (os.Stdout) and stderr (os.Stderr).
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Llongfile)
+
+	// Pass openDB() dsn from command-line flag.
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	// Close connection pool before the main() function exits.
+	defer db.Close()
 
 	// Initialize a new instance of application containing the dependencies
 	app := &application{
@@ -39,6 +52,18 @@ func main() {
 
 	// Running the HTTP server.
 	infoLog.Printf("Starting server on port %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
